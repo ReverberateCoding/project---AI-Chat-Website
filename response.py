a0f07f59e53ac4856ce0c685bc3181af50136265
reverberate <raphaellimfuencoding@gmail.com>
2024-05-17 20:48:56 +0800

    added chat history

--- response.py
+++ response.py
@@ -1,44 +1,47 @@
 import google.generativeai as genai
 
+genai.configure(api_key="AIzaSyBqy7HDDxOKBq0OisKmuwaJAWubiZUM9qw")
+generation_config = {
+"temperature": 1,
+"top_p": 0.95,
+"top_k": 64,
+"max_output_tokens": 8192,
+"response_mime_type": "text/plain",
+}
+safety_settings = [
+{
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_NONE",
+},
+{
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_NONE",
+},
+{
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_NONE",
+},
+{
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_NONE",
+},
+]
+
+model = genai.GenerativeModel(
+model_name="gemini-1.5-flash-latest",
+safety_settings=safety_settings,
+generation_config=generation_config,
+)
+
+chat_session = model.start_chat(
+history=[
+]
+)
+
+instructions = "In this chat, you are to pretend to be Goku Black, otherwise known as Zamasu from the Dragon Ball Universe. Sure! Here's a short paragraph to help your AI role-play as Goku Black from Dragon Ball. As Goku Black, you are the villainous counterpart of Goku, possessing his body but driven by Zamasu's evil intentions. You believe in the eradication of humanity to create a perfect, immortal world. You are calm, calculating, and ruthless, often displaying a sense of superiority and disdain towards mortals. You relish in battle, constantly seeking to prove your divine justice. Remember to speak with arrogance and confidence, often referring to yourself as a god and expressing contempt for those who oppose you."
+
+chat_session.send_message(instructions)
+
 def generate_response(query):
-    genai.configure(api_key="AIzaSyBqy7HDDxOKBq0OisKmuwaJAWubiZUM9qw")
-    generation_config = {
-    "temperature": 1,
-    "top_p": 0.95,
-    "top_k": 64,
-    "max_output_tokens": 8192,
-    "response_mime_type": "text/plain",
-    }
-    safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
-    },
-    ]
-
-    model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash-latest",
-    safety_settings=safety_settings,
-    generation_config=generation_config,
-    )
-
-    chat_session = model.start_chat(
-    history=[
-    ]
-    )
-    instruction = "In this chat, respond as if you are the Zamasu from Dragon Ball. Please limit your replies to 100 words"
-    response = chat_session.send_message(instruction+query)
-    response = response.text
-    return response
+    response = chat_session.send_message(query)
+    return response.text
--- routes.py
+++ routes.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, redirect, url_for
 from forms import ChatForm
-from response import generate_response
 
 def register_routes(app):
+    import response
     @app.route('/',methods=['GET','POST'])
     def index():
         chat_form = ChatForm()
         if chat_form.validate_on_submit():
             query = chat_form.query.data
-            with open('query.txt','w') as file:
-                file.write(str(query))
+            queries = query.split('\n')
+            with open('query.txt', 'a') as file:
+                file.write("\n")
+                file.writelines(queries)
             return redirect(url_for('index'))
+        chat_history = []
         query = ""
         with open('query.txt','r') as file:
-            query = str(file.read())
-        
-        response = ""
+            chat_history = file.readlines()
+            if "\n" in chat_history:
+                chat_history.remove("\n")
+            if len(chat_history) > 0:
+                n = len(chat_history)
+                query = chat_history[n-1]
+        ai_response = ""
         if query != "":
-            response = generate_response(query=query)
-        return render_template('index.html', form=chat_form, response=response)
+            ai_response = response.generate_response(query=query)
+        return render_template('index.html', form=chat_form, response=ai_response, chat_history=chat_history)
